1776a28222f9e617f9926f1d401d76c7ced18014
AidounNejma <guermachenejma@gmail.com>
2021-11-30 20:53:07 +0100

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,33 @@
 const pokedex = document.getElementById("pokedex")
 
 const fetchPokemon = () => {
-const promises = [];
+    const promises = [];
     for (let i = 1; i <= 150; i++) {
-            const url =`https://pokeapi.co/api/v2/pokemon-species/${i}`;
-            promises.push(fetch(url).then((res) => res.json()));
+        const url = `https://pokeapi.co/api/v2/pokemon-species/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
     }
     Promise.all(promises).then((results) => {
-        const pokemon = results.map((data) =>  {
-             let pokedata = {
-                description : data.flavor_text_entries.filter(entry => entry.language.name == "fr")[0]?.flavour_text,
-                name : data.names.filter(entry => entry.language.name == "fr")[0]?.name,
-
-                   id: data.id,
-                   /* 
+        const pokemon = results.map((data) => {
+            let pokedata = {
+                description: data.flavor_text_entries.filter(entry => entry.language.name == "fr")[0]?.flavour_text,
+                name: data.names.filter(entry => entry.language.name == "fr")[0]?.name,
+                id: data.id,
+                /* 
                 image: data.sprites['front_default'],
-                
                 type: data.types.map((type) => type.type.name),
                  */
-             }
+            }
 
-             return pokedata;
+            return pokedata;
         }
         );
+        displayPokemon(pokemon);
     });
-        for (let i = 1; i <= 150; i++) {
-        const url =https://pokeapi.co/api/v2/pokemon/${i};
-        promises.push(fetch(url).then((res) => res.json()));
-}
-Promise.all(promises).then((results) => {
-    const pokemon = results.map((data) => 
-    ({
-        name: data.name,
-        id: data.id,
-        image: data.sprites['front_default'],
-        type: data.types.map((type) => type.type.name)
-    }));
-    displayPokemon(pokemon);
-});
 };
 
-const displayPokemon = (pokemon) =>{
+const displayPokemon = (pokemon) => {
     console.table(pokemon);
-    const pokemonHTMLString = pokemon.map (pokeman => `
+    const pokemonHTMLString = pokemon.map(pokeman => `
     <li class="cards"
         data-name="${pokeman.name}"
         data-image="${pokeman.image}"
@@ -53,8 +38,25 @@ const displayPokemon = (pokemon) =>{
         <p class="card-type">${pokeman.type}</p>
     </li>
         `)
-        console.log(pokemonHTMLString);
-    pokedex.innerHTML = pokemonHTMLString.join(' ') ;
+    console.log(pokemonHTMLString);
+    pokedex.innerHTML = pokemonHTMLString.join(' ');
 };
 
 fetchPokemon();
+
+/*    
+    for (let i = 1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((data) =>
+        ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map((type) => type.type.name)
+        }));
+        displayPokemon(pokemon);
+    });
+*/
